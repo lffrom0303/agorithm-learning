eb07793588ef29526171b2d15635c717e08aa5f0
LF <958881587@qq.com>
2024-06-19 17:14:35 +0800

    feat: 盛最多水的容器

---
+++ 指针/盛最多水的容器.js
@@ -0,0 +1,15 @@
+var maxArea = function (height) {
+  let i = 0,
+    j = height.length - 1;
+  let maxArea = 0;
+  while (i < j) {
+    let currentArea = Math.min(height[i], height[j]) * (j - i);
+    maxArea = Math.max(maxArea, currentArea);
+    if (height[i] < height[j]) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+  return maxArea;
+};
